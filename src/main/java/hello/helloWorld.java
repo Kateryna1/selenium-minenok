0d3b8694c0152af143a84322c5102c356695afb2
Kateryna1 <ekaterina.minenok@gmail.com>
2015-03-10 20:18:16 +0200

    Project create

---
+++ src/main/java/hello/helloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by Kateryna.Minenok on 10.03.2015.
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
